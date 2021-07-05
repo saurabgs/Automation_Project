1e950e61be449eb5e11b764b46fe75d946bf7220
saurabgs <54474821+saurabgs@users.noreply.github.com>
2021-07-05 14:07:59 +0530

    Add files via upload

--- automation.sh
+++ automation.sh
@@ -4,6 +4,7 @@ dpkg --get-selections | grep apache
 sudo apt-get install apache2
 sudo systemctl status apache2
 sudo systemctl start apache2
+
 cd /tmp/
 tar saurab-httpd-logs$(date '+%d%m%Y-%H%M%S') ).tar /var/log/apache2/ 
 
@@ -14,4 +15,18 @@ cp /tmp/${myname}-httpd-logs-$(date '+%d%m%Y-%H%M%S')).tar \
 s3://${s3_bucket}/${myname}-httpd-logs-$(date '+%d%m%Y-%H%M%S')).tar
 
 
+FILE=/var/www/html/inventroy.html
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    touch inventory.html
+fi
 
+crontab -e
+0 1 * * * open ~/root/Automation_Project/automation.sh
+sudo nano /etc/crontab
+sudo nano /etc/crontab
+
+systemctl status cron
+sudo service cron start
+exit
